d16b0df09e61f1e70b541a4a17efb105828e0594
mange <mange@LAPTOP-117LVQ68>
2018-04-03 17:51:23 +0200

    Primer pas

---
+++ src/vehicles/Vehicle.java
@@ -0,0 +1,14 @@
+package vehicles;
+
+public class Vehicle {
+
+	private int cilindrada;
+	private int portes;
+	private int velocitat_actual = 0;
+	private static int velocitat_maxima = 120;
+	
+	public void Accelerar() {
+		if (velocitat_actual < velocitat_maxima) velocitat_actual += 10;
+	}
+	
+}
