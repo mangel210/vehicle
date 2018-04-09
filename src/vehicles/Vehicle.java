74cc60aa9284ee98b14199e23589744bc552af19
mange <mange@LAPTOP-117LVQ68>
2018-04-09 15:44:23 +0200

    Velocitat a zero

--- src/vehicles/Vehicle.java
+++ src/vehicles/Vehicle.java
@@ -6,7 +6,12 @@ public class Vehicle {
 	private int portes;
 	private int velocitat_actual = 0;
 	private static int velocitat_maxima = 120;
-	
+		
+	public Vehicle() {
+		super();
+		this.velocitat_actual = 0;
+	}
+
 	public void Accelerar() {
 		if (velocitat_actual < velocitat_maxima) velocitat_actual += 10;
 	}
