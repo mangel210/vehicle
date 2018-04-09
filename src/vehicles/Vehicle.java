0eabd3930875254f074309498e61117129f8adc4
mange <mange@LAPTOP-117LVQ68>
2018-04-09 16:09:39 +0200

    Afegir marxes

--- src/vehicles/Vehicle.java
+++ src/vehicles/Vehicle.java
@@ -5,11 +5,14 @@ public class Vehicle {
 	private int cilindrada;
 	private int portes;
 	private int velocitat_actual = 0;
+	private int marxa_actual = 0;
 	private static int velocitat_maxima = 120;
+	private static int marxes = 5;
 		
 	public Vehicle() {
 		super();
 		this.velocitat_actual = 0;
+		this.marxa_actual = 0;
 	}
 
 	public void Accelerar() {
@@ -20,4 +23,11 @@ public class Vehicle {
 		if (velocitat_actual > 0) velocitat_actual -= 10;
 	}
 	
+	public void pujaMarxa() {
+		if (marxa_actual < marxes) this.marxa_actual++;
+	}
+	
+	public void baixaMarxa() {
+		if (marxa_actual > 0) this.marxa_actual--;
+	}
 }
